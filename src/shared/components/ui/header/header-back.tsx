864c895314733991eef9ab8e2b048fd261190cab
dafariski77 <rskdafa@gmail.com>
2025-12-06 16:14:06 +0700

    chore: adjust header back color

--- src/shared/components/ui/header/header-back.tsx
+++ src/shared/components/ui/header/header-back.tsx
@@ -1,8 +1,13 @@
+import { colors } from "@/shared/themes";
 import { Octicons } from "@expo/vector-icons";
 import { useNavigation } from "expo-router";
 import { AppPressable } from "../app-pressable";
 
-export function HeaderBack() {
+interface HeaderBackProps {
+  color?: string;
+}
+
+export function HeaderBack({ color = colors.grayScale[900] }: HeaderBackProps) {
   const router = useNavigation();
   const canGoBack = router.canGoBack();
 
@@ -10,7 +15,7 @@ export function HeaderBack() {
 
   return (
     <AppPressable onPress={onPress}>
-      <Octicons name="arrow-left" size={28} />
+      <Octicons name="arrow-left" size={28} color={color} />
     </AppPressable>
   );
 }
